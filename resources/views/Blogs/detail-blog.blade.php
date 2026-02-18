9427dd9f3a7ee8838f3626625115aadc54ab3e8f
Eden Construction <wiselyhiring@gmail.com>
2026-02-18 16:14:43 +0500

    modified:   resources/views/Blogs/detail-blog.blade.php

--- resources/views/Blogs/detail-blog.blade.php
+++ resources/views/Blogs/detail-blog.blade.php
@@ -16,7 +16,7 @@
             title="{{ trim($blog->image_title) }}" style="width: 100%; height: 460px; object-fit: cover;" />
 
 
-        <h1 class="mt-3 mb-2" style="font-size: 2rem; font-weight: 400;">{{ $blog->title }}</h1>
+        <h1 class="mt-3 mb-2" style=" font-weight: bold;">{{ $blog->title }}</h1>
         {!! $blog->body !!}
     </div>
 </main>
